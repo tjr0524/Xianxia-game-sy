792c190ac4e2d56b240e7d53e81698f3305f7954
tjr0524 <134980365+tjr0524@users.noreply.github.com>
2026-09-18 08:10:58 +0900

    Bump canonical test build to 11.47.0

--- runtime_kernel_v11_45.js
+++ runtime_kernel_v11_45.js
@@ -1,8 +1,8 @@
 (()=>{
 'use strict';
 
-const BUILD='11.46.0';
-const SESSION_KEY='xianxia_runtime_safety_11_46_test';
+const BUILD='11.47.0';
+const SESSION_KEY='xianxia_runtime_safety_11_47_test';
 
 function setCanonicalBuild(){
   try{
